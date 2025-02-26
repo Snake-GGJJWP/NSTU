da7550445493d72545690f7a3ed1fe753b5c94eb
Snake-GGJJWP <vladiksnake@gmail.com>
2025-02-26 08:35:23 +0600

    -- list done

--- Lab1_SBList/List.c
+++ Lab1_SBList/List.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 #include "List.h"
 
-// Construct a single integer list node.
+// Construct a single integer list nextNode.
 IList* newIListNode(int val) {
 	IList* node = (IList*)malloc(sizeof(IList));
 	node->val = val;
@@ -25,15 +26,58 @@ IList* newIList(const int* arr, int len) {
 	return headNode;
 }
 // Insert integer 'val' at index 'ind' in 'list'. Return 0 on failure, 1 on success
-char ilistInsert(IList* list, int val, int ind) {
-	IList* newNode;
+IList* ilistInsert(IList* list, int val, int ind) {
+	IList* newNode = newIListNode(val);
+	IList* nextNode = ilistGetNode(list, ind); // nextNode at index 'ind'
+	IList* prevNode = ilistGetNode(list, ind - 1); // prevNode at index 'ind - 1'
+
+	if (nextNode == NULL && prevNode == NULL) return NULL;
+
+	if (nextNode != NULL) newNode->next = nextNode;
+	if (prevNode != NULL) prevNode->next = newNode;
+	else return newNode; // if the value inserted in the head, we should change the head
+
+	return list;
 }
 // Get value at index 'ind' in 'list'. Return value if there's any, INT_MIN on failure
-int ilistGetValue(const IList* list, int ind) {}
-// Get list node at index 'ind' in 'list'. Return node if there's any, NULL on failure
-IList* ilistGetNode(const IList* list, int ind) {}
-// Get 'list' length. Return length on success, -1 on failure.
-int ilistLength(const IList* list) {}
+int ilistGetValue(const IList* list, int ind) {
+	IList* node = ilistGetNode(list, ind);
+
+	if (node == NULL) return INT_MIN;
+
+	return node->val;
+}
+// Get list nextNode at index 'ind' in 'list'. Return nextNode if there's any, NULL on failure
+IList* ilistGetNode(const IList* list, int ind) {
+	if (ind < 0) return NULL;
+
+	IList* node = list;
+	for (int i = 0; i < ind; node = node->next) {
+		if (node == NULL) return NULL;
+		i++;
+	}
+
+	return node;
+}
+// Get 'list' length.
+int ilistLength(const IList* list) {
+	int count = 0;
+	for (IList* node = list; node != NULL; node = node->next)
+		count++;
+	return count;
+}
+
+// Remove node at index 'ind' from 'list'. Return 0 on failure, 1 on success
+IList* ilistRemove(IList* list, int ind) {
+	IList* nextNode = ilistGetNode(list, ind + 1); // nextNode at index 'ind + 1'
+	IList* prevNode = ilistGetNode(list, ind - 1); // prevNode at index 'ind - 1'
+
+	if (ilistGetNode(list, ind) == NULL) return NULL; // if the node we remove is not present, return NULL
+	if (prevNode == NULL) return nextNode; // if head is removed return the new head
+	
+	prevNode->next = nextNode; // doesn't matter if nextNode is actual node or NULL
+	return list;
+}
 
 // WIP
 char* toString(const IList* list) {
@@ -44,7 +88,10 @@ char* toString(const IList* list) {
 }
 
 void printList(const IList* list) {
+	printf("[");
 	for (IList* node = list; node != NULL; node = node->next) {
-		printf("%d ", node->val);
+		if (node->next != NULL) printf("%d, ", node->val);
+		else printf("%d", node->val);
 	}
+	printf("]\n");
 }
--- Lab1_SBList/main.c
+++ Lab1_SBList/main.c
@@ -2,8 +2,29 @@
 #include "List.h"
 
 int main() {
-	int testArr[5] = { 1, 2, 3, 4, 5 };
-	IList* list = newIList(testArr, 6);
+	int testArr[6] = { 1, 2, 3, 4, 5, 6 };
+	IList* list = newIList(testArr, 3);
 	printList(list);
+
+	// TEST getLength
+	int len = ilistLength(list);
+	printf("\nlen: %d\n", len);
+
+	// TEST getNode
+	IList* node = ilistGetNode(list, 2);
+	printList(node);
+
+	// TEST getValue
+	int val = ilistGetValue(list, 0);
+	printf("\nvalue: %d\n", val);
+
+	// TEST insert
+	// IList* head = ilistInsert(list, 6, -1);
+	// printList(head);
+
+	// TEST remove
+	IList* head = ilistRemove(list, -1);
+	printList(head);
+
 	return 0;
 }
